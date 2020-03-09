e1cdc151d28816c7b9828e56a5986bf0a8c6a9de
lincmg <lincolnmgijima@gmail.com>
2020-03-09 02:23:27 +0200

    I do not get it why test should fail.

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,25 +1,31 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-class AnimalTests {
+class AnimalTest {
+
+    Dog dog = new Dog();
     Cat cat = new Cat();
-    Dog dog= new Dog();
+
+    void TestDogSound() {
+    }
+
     @Test
-    void TestDogSound(){
-        assertEquals("Bark",dog.sound());
+    void TestDogEats() {
+        assertEquals("food".toLowerCase(),dog.eat().toLowerCase(),"Does dog eat Food");
+        assertEquals("food".toLowerCase(),dog.eat().toLowerCase(),"Does dog eat food");
     }
     @Test
-    void TestDogEats(){
-       assertEquals("Food", dog.eat());
-    }
-    @Test
-    void TestCatSound(){
-        assertEquals("Meow",cat.sound());
+    void TestCatSound() {
+        assertEquals("Barkark".toLowerCase(),cat.sound().toLowerCase(),"Does cat Barkark");
+        assertEquals("Meow".toLowerCase(),cat.sound().toLowerCase(),"Does cat Meow");
 
     }
     @Test
-    void TestCatEats(){
-       assertEquals("Food",cat.eat());
+    void TestCatEats() {
+        assertEquals("meat".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat meat");
+        assertEquals("Food".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat Food");
+        assertEquals("food".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat food");
     }
+
 }
