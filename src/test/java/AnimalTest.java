37518c2f6e2f2ef538e9696c7a99c1dab71f42e6
lincmg <lincolnmgijima@gmail.com>
2020-03-09 02:45:02 +0200

    Crazy Test case

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -7,25 +7,36 @@ class AnimalTest {
     Dog dog = new Dog();
     Cat cat = new Cat();
 
-    void TestDogSound() {
+
+    @Test
+    void TestDogSound(){
+        //No instruction on what to test.
     }
 
     @Test
-    void TestDogEats() {
-        assertEquals("food".toLowerCase(),dog.eat().toLowerCase(),"Does dog eat Food");
-        assertEquals("food".toLowerCase(),dog.eat().toLowerCase(),"Does dog eat food");
-    }
-    @Test
-    void TestCatSound() {
-        assertEquals("Barkark".toLowerCase(),cat.sound().toLowerCase(),"Does cat Barkark");
-        assertEquals("Meow".toLowerCase(),cat.sound().toLowerCase(),"Does cat Meow");
-
-    }
-    @Test
-    void TestCatEats() {
-        assertEquals("meat".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat meat");
-        assertEquals("Food".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat Food");
-        assertEquals("food".toLowerCase(),cat.eat().toLowerCase(),"Does cat eat food");
+    void TestDogEats(){
+        assertEquals("Food",dog.eat(),"Does dog eat Food");// Test should pass
+        assertEquals("food",dog.eat(),"Does dog eat food");// Test should fail,
     }
 
+    @Test
+    void TestCatSound(){
+        assertEquals("Barkark",cat.sound(),"Does cat Barkark should");//Test should fail
+        assertEquals("Meow",cat.sound(),"Does cat Meow should"); //Test should pass
+    }
+
+    @Test
+    void TestCatEats(){
+        assertEquals("meat",cat.eat(),"Does cat eat meat"); //Test should fail
+        assertEquals("Food",cat.eat(),"Does cat eat Food"); //Test should pass
+        assertEquals("food",cat.eat(),"Does cat eat food"); //Test should fail
+    }
+
+    @Test
+    void sound() {
+    }
+
+    @Test
+    void eat() {
+    }
 }
